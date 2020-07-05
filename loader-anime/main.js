612d158d552facb8d61b790ca53c45e42c374a6e
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-07-05 14:20:37 +0200

    animation dotted circle

--- loader-anime/main.js
+++ loader-anime/main.js
@@ -2,12 +2,36 @@ class SpinningDots extends HTMLElement {
     constructor () {
         super();
         const width = 50;
-        // const circleRadius = 2;
+        const circleRadius = 4;
+        const circles = 8;
         const root = this.attachShadow({mode: 'open'});
 
         root.innerHTML = `<div>
             ${this.buildStyle(width)}
+            ${this.buildCircles(width, circles, circleRadius)}
         </div>`;
+    }
+
+    /**
+     * Construitun SVG contenant les différents cercles
+     * @param {number} w largeur SVG
+     * @param {number} n Nombre de cercles
+     * @param {number} r Rayon de chaque cercle
+     * @returns {string}
+     */
+    buildCircles(w, n, r) {
+        let dom = `<svg class="circles" width="${w}" height="${w}" viewBox="0 0 ${w} ${w}">`;
+        const radius = (w / 2 - r);
+
+        for (let i = 0; i < n; i++) {
+            const angle = i * (Math.PI * 2) / n;
+            const x = radius * Math.sin(angle) + w / 2;
+            const y = radius * Math.cos(angle) + w / 2;
+
+            dom += `<circle cx="${x}" cy="${y}" r="${r}" fill="currentColor"/>`;
+        }
+
+        return dom + `</svg>`;
     }
 
     /**
@@ -17,10 +41,23 @@ class SpinningDots extends HTMLElement {
      */
     buildStyle(w) {
         return `<style>
+            :host {
+                display: inline-block;
+            }
+
             div {
                 width: ${w}px;
                 height: ${w}px;
                 background: chartreuse;
+            }
+
+            .circles {
+                animation: spin 16s linear infinite;
+            }
+
+            @keyframes spin {
+                from {transform: rotate(0 deg)}
+                to {transform: rotate(360deg)}
             }
         </style>`
     }
